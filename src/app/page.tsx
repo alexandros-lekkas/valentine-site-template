8929b95cd57bf0e22f871126bfaddad0c0444c4d
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-02-04 21:41:12 -0600

    Added scroll progress, wow magic UI is cool - now scaffolding what I want the site to look like on paper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-"use client";
+import * as React from "react";
 
-import { Heart } from "@phosphor-icons/react";
+import { Heart } from "@phosphor-icons/react/dist/ssr";
+
+import { ScrollProgress } from "@/components/ui/scroll-progress";
 
 export default function Home() {
   return (
     <>
+      <ScrollProgress className="from-red-200 via-pink-600 to-red-600"/>
       <div className="container mx-auto">
         <Heart className="w-72 h-72 text-red-500 hover:animate-ping" weight="fill" />
         <h1 className="text-2xl fo">Valentines Site Template</h1>
