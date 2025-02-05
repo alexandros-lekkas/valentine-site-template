a52c40fc79e8525d9ea02cc34eac263411416e59
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-02-04 22:39:46 -0600

    Created moments

--- src/components/page/moments.tsx
+++ src/components/page/moments.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 
 import { Marquee } from "../ui/marquee";
+import { BlurFade } from "../ui/blur-fade";
 
-const moments = [
-  {
-    img: "/moments/1.webp",
-    caption: "I Love You",
-  },
-];
+import { cn } from "@/lib/utils";
+
+import { moments } from "@/config";
 
 const firstRow = moments.slice(0, moments.length / 2);
 const secondRow = moments.slice(moments.length / 2);
@@ -15,8 +13,12 @@ const secondRow = moments.slice(moments.length / 2);
 function MomentCard({ img, caption }: { img: string; caption: string }) {
   return (
     <div className="relative w-full h-64 md:h-96">
-      <img src={img} alt={caption} className="object-cover w-full h-full" />
-      <div className="absolute bottom-0 left-0 right-0 p-4 text-xl font-bold text-white bg-black bg-opacity-50">
+      <img
+        src={img}
+        alt={caption}
+        className="object-cover w-full h-full rounded-xl"
+      />
+      <div className="absolute bottom-0 left-0 right-0 p-4 text-xl font-bold text-white rounded-lg bg-gradient-to-t from-black/60 to-transparent">
         {caption}
       </div>
     </div>
@@ -25,18 +27,29 @@ function MomentCard({ img, caption }: { img: string; caption: string }) {
 
 export function Moments() {
   return (
-    <div className="container mx-auto justify-center py-20">
-      <Marquee pauseOnHover className="[--duration:20s]">
-        {firstRow.map((moment, i) => (
-          <MomentCard key={i} {...moment} />
-        ))}
-      </Marquee>
+    <div className="container mx-auto justify-center">
+      <BlurFade delay={0.25} inView>
+        <h2 className="mb-8 text-4xl font-bold text-center">Moments</h2>
+      </BlurFade>
 
-      <Marquee reverse pauseOnHover className="[--duration:20s]">
-        {secondRow.map((moment, i) => (
-          <MomentCard key={i} {...moment} />
-        ))}
-      </Marquee>
+      <BlurFade delay={0.5} inView>
+        <div className="relative flex w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background">
+          <Marquee pauseOnHover className="[--duration:90s]">
+            {firstRow.map((moment, i) => (
+              <MomentCard key={i} {...moment} />
+            ))}
+          </Marquee>
+
+          <Marquee reverse pauseOnHover className="[--duration:90s]">
+            {secondRow.map((moment, i) => (
+              <MomentCard key={i} {...moment} />
+            ))}
+          </Marquee>
+
+          <div className="pointer-events-none absolute inset-y-0 left-0 w-1/12 bg-gradient-to-r from-white dark:from-background"></div>
+          <div className="pointer-events-none absolute inset-y-0 right-0 w-1/12 bg-gradient-to-l from-white dark:from-background"></div>
+        </div>
+      </BlurFade>
     </div>
   );
 }
---
+++ src/config.ts
@@ -0,0 +1,38 @@
+export const moments = [
+  {
+    img: "/moments/1.webp",
+    caption: "Remember when we first met?",
+  },
+  {
+    img: "/moments/2.webp",
+    caption: "You were so beautiful",
+  },
+  {
+    img: "/moments/3.webp",
+    caption: "You supported me always. Even when I was a mess",
+  },
+  {
+    img: "/moments/4.webp",
+    caption: "JavaScript vs. you. You win...",
+  },
+  {
+    img: "/moments/5.webp",
+    caption: "Wow. Just wow.",
+  },
+  {
+    img: "/moments/6.webp",
+    caption: "You helped me find the errors in my code and in my life",
+  },
+  {
+    img: "/moments/7.webp",
+    caption: "I remember moving in to our first Next project.",
+  },
+  {
+    img: "/moments/8.webp",
+    caption: "Even through our challenging moments...",
+  },
+  {
+    img: "/moments/9.png",
+    caption: "ts 💔",
+  },
+];
